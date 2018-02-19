5038cdacc25b14b62fffc3b0fedec27fe542f644
James Harper <james.harper.us@gmail.com>
2018-02-18 22:42:34 -0700

    Plotting in MCA

--- functions.R
+++ functions.R
@@ -291,7 +291,7 @@ correspondence_analysis = function(data, metric1, metric2) {
   sink()
   sink(type = "message")
   
-  # Generate plots and store in list
+  # Generate plots and store in variables
   plot1 = fviz_ca_biplot(freqs.ca, map = "symbiplot", 
                                     arrow = c(TRUE, TRUE), repel = TRUE)
   plot2 = fviz_screeplot(freqs.ca, addlabels = TRUE, 
@@ -333,44 +333,47 @@ correspondence_analysis = function(data, metric1, metric2) {
 multiple_correspondence_analysis = function(data, quali_sup, quanti_sup) {
   library(FactoMineR)
   
+  # Perform multiple correspondence analysis
+  results = MCA(data, quali.sup = quali_sup, quanti.sup = quanti_sup)
+  
   # Create file name and plot name variables
-  name = paste("ca_", names(data)[[metric1]], "_", names(data)[[metric2]], 
-               sep = "")
-  plot_name = paste(names(data)[[metric1]], "_", names(data)[[metric2]],
-                    sep = "")
+  name = "mca"
+  plot_name = name
   
   # Start sending text output to text file in folder
   file1 = file(paste(getwd(), "/output/", name, ".txt", sep = ""))
   sink(file1, append = TRUE)
   sink(file1, append = TRUE, type = "message")
   
-  # Perform multiple correspondence analysis
-  results = MCA(data, quali.sup = quali_sup, quanti.sup = quanti_sup)
-  
-  
+  # Print results from multiple correspondence analysis
   summary(results, ncp = 3, nbelements = Inf)
   dimdesc(results)
-  # pdf(paste(getwd(),"/output/plot.pdf", sep = ""))
-  plot(results, label = c("var","quali.sup"), cex = 0.7)
-  plot(results, invisible = c("var","quali.sup"), cex = 0.7)
-  plot(results, invisible = c("ind","quali.sup"), autoLab = "y", cex = 0.7, title = "Active Categories")
-  plot(results, invisible = c("ind","quali.sup"), autoLab = "y", cex = 0.7, title = "Active Categories", selectMod = "contrib 20")
-  plot(results, invisible = c("ind","quali.sup"), cex = 0.7, title = "Active Categories")
-  plot(results, invisible = c("ind","var"), autoLab = "y", cex = 0.7, title = "Supplementary Categories")
-  plot(results, invisible = "ind", autoLab = "y", cex = 0.7, selectMod = "cos2 10")
-  plot(results, invisible = "ind", autoLab = "y", cex = 0.7, selectMod = "contrib 20")
-  plot(results, invisible = c("var","quali.sup"), autoLab = "y", cex = 0.7, select = "cos2 10")
-  plot(results, autoLab = "y", cex = 0.7, selectMod = "cos2 20", select = "cos2 10")
-  plot(results, choix = "var", xlim = c(0,0.6), ylim = c(0,0.6))
-  plot(results, choix = "var", xlim = c(0,0.6), ylim = c(0,0.6), invisible = c("ind","quali.sup"))
-  plot(results, invisible = c("var","quali.sup"), cex = 0.7, select = "contrib 20", axes = 3:4)
-  plot(results, invisible = c("ind"), cex = 0.7, select = "contrib 20", axes = 3:4)
-  plotellipses(results, keepvar = c(1:4))
+  
+  # Stop saving text output to file
+  sink()
+  sink(type = "message")
+  
+  # Generate plots and store in variables
+  plot1 = recordPlot(plot(results, label = c("var","quali.sup"), cex = 0.7))
+  plot2 = recordPlot(plot(results, invisible = c("var","quali.sup"), cex = 0.7))
+  plot3 = recordPlot(plot(results, invisible = c("ind","quali.sup"), autoLab = "y", cex = 0.7, title = "Active Categories"))
+  plot4 = recordPlot(plot(results, invisible = c("ind","quali.sup"), autoLab = "y", cex = 0.7, title = "Active Categories", selectMod = "contrib 20"))
+  plot5 = recordPlot(plot(results, invisible = c("ind","quali.sup"), cex = 0.7, title = "Active Categories"))
+  plot6 = recordPlot(plot(results, invisible = c("ind","var"), autoLab = "y", cex = 0.7, title = "Supplementary Categories"))
+  plot7 = recordPlot(plot(results, invisible = "ind", autoLab = "y", cex = 0.7, selectMod = "cos2 10"))
+  plot8 = recordPlot(plot(results, invisible = "ind", autoLab = "y", cex = 0.7, selectMod = "contrib 20"))
+  plot9 = recordPlot(plot(results, invisible = c("var","quali.sup"), autoLab = "y", cex = 0.7, select = "cos2 10"))
+  plot10 = recordPlot(plot(results, autoLab = "y", cex = 0.7, selectMod = "cos2 20", select = "cos2 10"))
+  plot11 = recordPlot(plot(results, choix = "var", xlim = c(0,0.6), ylim = c(0,0.6)))
+  plot12 = recordPlot(plot(results, choix = "var", xlim = c(0,0.6), ylim = c(0,0.6), invisible = c("ind","quali.sup")))
+  plot13 = recordPlot(plot(results, invisible = c("var","quali.sup"), cex = 0.7, select = "contrib 20", axes = 3:4))
+  plot14 = recordPlot(plot(results, invisible = c("ind"), cex = 0.7, select = "contrib 20", axes = 3:4))
+  plot15 = recordPlot(plotellipses(results, keepvar = c(1:4)))
   
   # Save plots to PDF
   ggexport(plotlist = list(plot1, plot2, plot3, plot4, plot5, plot6, plot7,
                            plot8, plot9, plot10, plot11, plot12, plot13,
-                           plot14, plot15, plot16, plot17, plot18), 
+                           plot14, plot15), 
            filename = paste(getwd(), "/output/", name, ".pdf", sep = ""))
 }
 
