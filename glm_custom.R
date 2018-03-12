54e4cb7e073c80a30c9282a89a970ac20e651009
James Harper <james.harper.us@gmail.com>
2018-03-12 16:14:40 -0600

    Improved algorithm

--- glm_custom.R
+++ glm_custom.R
@@ -34,7 +34,8 @@ for (i in iter) {
   
   # Create training (80%) and testing (20%) sets using random sampling
   indices_all = 1:len
-  indices_train = sort(sample(x = indices_all, size = round(0.7*len, 0), replace = F), decreasing = F)
+  indices_train = sort(sample(x = indices_all, size = round(0.7*len, 0), 
+                              replace = F), decreasing = F)
   indices_test = indices_all[!(indices_all %in% indices_train)]
   # data.frame(indices_train[1:100], indices_test[1:100])
   train = data_sub[indices_train,]
@@ -161,3 +162,120 @@ plot3 = ggplot(data, aes(x = IntndPitFullDes)) + geom_bar(); plot3
 # exp(confint(model)) # 95% CI for exponentiated coefficients
 # predict(model, type="response") # predicted values
 # residuals(model, type="deviance") # residuals
+
+
+###############################################################################
+# PERFORM GENERALIZED LINEAR MODELING - IntndPitFull2pit or IntndPitFullEmpSlf
+###############################################################################
+# Subset data to include only rows with responses to IntndPitFullDes
+summary(data); length(data[,1])
+data_sub = subset(data, !is.na(IntndPitFullEmpSlf))
+summary(data_sub); length(data_sub[,1])
+len = length(data_sub[,1])
+
+# Run generalized linear model repeatedly to calculate average accuracy
+iter = 1:100
+accuracy = rep(0, times = length(iter))
+for (i in iter) {
+  
+  # Create training (80%) and testing (20%) sets using random sampling
+  indices_all = 1:len
+  indices_train = sort(sample(x = indices_all, size = round(0.7*len, 0), 
+                              replace = F), decreasing = F)
+  indices_test = indices_all[!(indices_all %in% indices_train)]
+  # data.frame(indices_train[1:100], indices_test[1:100])
+  train = data_sub[indices_train,]
+  test = data_sub[indices_test,]
+  # summary(train); length(train[,1])
+  # summary(test); length(test[,1])
+  # hist(indices_train); hist(indices_test)
+  
+  # Summarize training data
+  # for (j in 1:length(train)) {
+  #   plot(train[, j], main = colnames(train)[j],
+  #        ylab = "Count", col = "steelblue")
+  # }
+  
+  # List names in data
+  # names(data_sub)
+  # [1] "LBO"                  "Prov"                 "CGend"                "IDPoor"              
+  # [5] "LivRP"                "VillOD"               "AdltUseLat"           "ChldUseLat"          
+  # [9] "InfLatDump"           "IntndPitFull"         "Satis"                "Rec"                 
+  # [13] "SatisSup"             "RecSup"               "Yr"                   "Mnth"                
+  # [17] "RDefBefor_BshFld"     "RDefBefor_RivPnd"     "RDefBefor_NeiToi"     "RDefBefor_Othr"      
+  # [21] "RDefBefor_NAAlwysToi" "IntndChng_Shltr"      "IntndChng_Shwr"       "IntndChng_HndWsh"    
+  # [25] "IntndChng_WtrRes"     "IntndChng_2pit"       "IntndChng_Othr"       "IntndChng_NAAlwysToi"
+  # [29] "IntndPitFullDes"
+  
+  # Test individual variables predictive power of IntndPitFullDes
+  # model = glm(formula = IntndPitFullDes ~ RDefBefor_BshFld,
+  #             data = train,
+  #             family = binomial(link = "logit"),
+  #             na.action = na.omit); summary(model); anova(model, test = "Chisq")
+  # names(train)
+  # p<.05: LBO, Prov, IDPoor, InfLatDump, Satis, Rec, SatisSup, RecSup, Yr, 
+  #        Mnth, RDefBefor_BshFld, RDefBefor_NeiToi, IntndChng_Shwr, 
+  #        IntndChng_HndWsh, IntndChng_WtrRes, IntndChng_2pit
+  # p>.05: CGend, LivRP, VillOD, AdltUseLat, ChldUseLat, RDefBefor_RivPnd, 
+  #        IntndChng_Shltr
+  
+  # Test model with all variables to predict IntndPitFullDes
+  # names(train)
+  # model = glm(formula = IntndPitFullDes ~ Prov + CGend + IDPoor + LivRP + 
+  #               VillOD + AdltUseLat + ChldUseLat + InfLatDump + Satis + Rec +
+  #               SatisSup + RecSup + Yr + Mnth + RDefBefor_BshFld +
+  #               RDefBefor_RivPnd + RDefBefor_NeiToi + IntndChng_Shltr +
+  #               IntndChng_Shwr + IntndChng_HndWsh + IntndChng_WtrRes +
+  #               IntndChng_2pit,
+  #             data = train,
+  #             family = binomial(link = "logit"),
+  #             na.action = na.omit); summary(model); anova(model, test = "Chisq")
+  # Removed due to low interest: LBO
+  # p<.05: Prov, VillOD, Satis, SatisSup, RecSup, RDefBefor_BshFld,
+  #        IntndChng_Shwr, IntndChng_HndWsh, IntndChng_WtrRes,
+  #        IntndChng_2pit
+  # p<.10: IDPoor
+  # p>.10: CGend, LivRP, AdltUseLat, ChldUseLat, InfLatDump, Rec, Yr, Mnth, 
+  
+  # Refine model to include only variables that 1) significantly related to
+  # IntndPitFull, adn 2) reduce residual deviance markedly
+  model = glm(formula = IntndPitFullEmpSlf ~ Prov + IDPoor + VillOD + Satis + 
+                SatisSup + RecSup + Yr + Mnth + RDefBefor_BshFld +
+                IntndChng_Shwr + IntndChng_HndWsh + IntndChng_WtrRes +
+                IntndChng_2pit,
+              data = train,
+              family = binomial(link = "logit"),
+              na.action = na.omit)
+  
+  # Analyze model fit
+  # summary(model)
+  # anova(model, test = "Chisq")
+  # pR2(model)
+  
+  # Test predictive power of model on testing data
+  predict_results = function(model, test, type) {
+    fitted.results = predict(model, newdata = test, type = type)
+    return(fitted.results)
+  }
+  fitted.results = try(predict_results(model, test, "response"), silent = T)
+  if (inherits(fitted.results, "try-error")) {
+    next
+  }
+  fitted.results = ifelse(fitted.results > 0.5, 1, 0)
+  # data.frame(test$IntndPitFullDes[!is.na(fitted.results)], 
+  #            fitted.results[!is.na(fitted.results)])
+  misclass.error = mean(fitted.results[!is.na(fitted.results)] != 
+                          test$IntndPitFullDes[!is.na(fitted.results)])
+  accuracy[i] = 1 - misclass.error
+  
+}
+head(accuracy)
+# hist(accuracy)
+mean(accuracy[accuracy != 0])
+
+# Visualization
+plot1 = ggplot(data, aes(x = IntndPitFullDes, 
+                         y = Prov)) + geom_count(); plot1
+plot2 = ggplot(data, aes(x = IntndPitFullDes, 
+                         y = Prov, color = CGend)) + geom_count(); plot2
+plot3 = ggplot(data, aes(x = IntndPitFullDes)) + geom_bar(); plot3
